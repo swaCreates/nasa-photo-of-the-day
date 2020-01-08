5bfa40d10f46a2ba07add3036881374e4983a992
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-01-07 20:05:40 -0500

    I received the data from my get request

--- src/components/ApodComponents/ApodCard.js
+++ src/components/ApodComponents/ApodCard.js
@@ -1,8 +1,6 @@
 import React, {useState, useEffect} from 'react';
-import axios from 'axios';
 
 function ApodCard() {
-
 
 
     return (
--- src/components/ApodComponents/ApodList.js
+++ src/components/ApodComponents/ApodList.js
@@ -4,7 +4,19 @@ import axios from 'axios';
 
 function ApodList() {
 
-    
+    const [photo, setPhoto]= useState([]);
+
+    useEffect(() => {
+        axios
+        .get('https://api.nasa.gov/planetary/apod?api_key=sKf1PeqzFRWl4kDX1QTznId3B1WBhr2o3GalEcdo')
+        .then(res => {
+            console.log(res.data);
+            setPhoto(res.data);
+        })
+        .catch(err => {
+            console.log('This is in error: ', err);
+        })
+    }, []);
 
     return (
         <div>
