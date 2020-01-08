6a5263d5da773e38f4bdbb01aac280a433b40b27
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-01-07 21:19:55 -0500

    having trouble rendering data

--- src/components/ApodComponents/ApodCard.js
+++ src/components/ApodComponents/ApodCard.js
@@ -1,11 +1,21 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 
-function ApodCard() {
+function ApodCard(props) {
 
 
     return (
         <div className='apod-card'>
-            
+            <div className='img'>
+                {props.url}
+            </div>
+            <div>
+                <h1>Title: {props.title}</h1>
+                <h2>Author: {props.name}</h2>
+                <p>{props.description}</p>
+            </div>
+            <div>
+                Date: {props.date}
+            </div>
         </div>
     )
 }
--- src/components/ApodComponents/ApodList.js
+++ src/components/ApodComponents/ApodList.js
@@ -11,8 +11,8 @@ function ApodList() {
         axios
         .get('https://api.nasa.gov/planetary/apod?api_key=sKf1PeqzFRWl4kDX1QTznId3B1WBhr2o3GalEcdo')
         .then(res => {
-            console.log(res.data);
             setPhoto(res.data);
+            console.log(res.data)
         })
         .catch(err => {
             console.log('This is in error: ', err);
@@ -21,7 +21,7 @@ function ApodList() {
 
     return (
         <div className='apod-list'>
-            {photo.map((index, data) => {
+            {photo.map((data, index) => {
                 <ApodCard key={index} name={data.copyright} date={data.date} description={data.explanation} title={data.title} url={data.url} />
             })}
         </div>
