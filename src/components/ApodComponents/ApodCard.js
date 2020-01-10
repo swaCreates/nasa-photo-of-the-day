e04b24a51ea0e960c7a4e3a66761f728527a406e
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-01-09 20:56:17 -0500

    adding styled components

--- src/components/ApodComponents/ApodCard.js
+++ src/components/ApodComponents/ApodCard.js
@@ -1,24 +1,62 @@
 import React from 'react';
+import Moment from 'react-moment';
+import {Card, Button, Jumbotron} from 'reactstrap';
+
 
 function ApodCard(props) {
 
 
+    // Trying to create button to change the date. I need to refactor my whole project to implement this inside of ApodList file, instead of here.
+
+    // useEffect(()=> {
+    //     const dateChange= (e)=> {
+    //         e.persist()
+    //         console.log(e)
+    //         let date= (e.target.valueAsDate)
+    //         date= date.toUTCString();
+    //         let formattedDate= moment(date).format('YYYY-MM-DD')
+    //         console.log(formattedDate);
+
+    //         axios
+    //         .get(`https://api.nasa.gov/planetary/apod?api_key=sKf1PeqzFRWl4kDX1QTznId3B1WBhr2o3GalEcdo&date=${formattedDate}`)
+    //         .then(res => setPhoto(res.data))
+    //         .catch(err => console.log('This is my error: ', err));
+            
+            
+    //     }
+    // }, [])
+
+
     return (
-        <div>
+        <Card className='reactCard'>
+            <Jumbotron className='jumbo'>
+                <h1 className="display-4">Welcome to NASA Photo Of the Day</h1>
+            </Jumbotron>
             <div className='apod-card'>
                 <div className='img'>
-                    <img src={props.url} alt='image of flaming star nebula' />
+                    <img src={props.url} alt= {props.title} />
                 </div>
                 <div>
-                    <h1><span>Topic:</span> {props.title}</h1>
-                    <h2><span>Author:</span> {props.name}</h2>
+                    <h1>Topic: {props.title}</h1>
+                    <h2>Author: {props.name}</h2>
                     <p>{props.description}</p>
                 </div>
                 </div>
             <div>
-                Date Published: {props.date}
+                <div className='btn'>
+                    <Button color='primary' className='btn-1'><span role='img'>👍</span></Button>
+                    <Button color='primary' className='btn-2'><span role='img'>👎</span></Button>
+                </div>
+                Date Published: 
+                <Moment format='MM-DD-YYYY'>
+                 {props.date}
+                </Moment>
+                <form>
+                    <input /*onChange={dateChange}*/ type='date'></input>
+                </form>
             </div>
-        </div>
+            
+        </Card>
     )
 }
 
--- src/components/ApodComponents/ApodList.js
+++ src/components/ApodComponents/ApodList.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import ApodCard from './ApodCard';
 
 
+
 function ApodList() {
 
     const [photo, setPhoto]= useState([]);
